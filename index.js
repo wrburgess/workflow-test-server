5ca4ab312d61e7573ff98455408b32a5f300f162
Randy Burgess <wrburgess@gmail.com>
2019-10-01 09:30:04 -0500

    removing cors

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const http = require('http');
 const express = require('express');
+const cors = require('cors');
 const workflowsRouter = require('./routes/workflowsRouter');
+const PORT = process.env.PORT || 3000;
+// var corsOptions = {
+//   origin: 'http://localhost:1234',
+//   optionsSuccessStatus: 200
+// };
 
 let app = express();
-var port = process.env.PORT || 3000;
-
+// app.use(cors(corsOptions));
 app.use(workflowsRouter);
 
 let server = http.createServer(app);
-server.listen(port);
+server.listen(PORT, () => {
+  console.log(`SERVER: Listening on PORT ${PORT}`);
+});
